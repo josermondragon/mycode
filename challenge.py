a0f4614e9ae019d5397728026eb74b43456608c1
Jose Mondragon <josermondragon@users.noreply.github.com>
2022-05-17 19:59:31 +0000

    join and split

---
+++ challenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def chall1():
+    name = input("type your name: ")
+    day = input("type the day we are in: ")
+    print("Hello!",name," Happy",day)
+    print(f"Hello, {name}! Happy {day}!")
+chall1()
