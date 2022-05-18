8d6028c44fc2d4e770650ccb3357a4439bd7776e
josermondragon <j.mondragon@hotmail.com>
2022-05-18 19:55:10 +0000

    learning about dictionaries

---
+++ superheroes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+Freakzoid={'Real Name':'Remy LeBeau','First Appearance':'Uncanny X-Men Annual #14 (July, 1990), Uncanny X-Men #266 (August, 1990)','Creators':['Chris Claremont','Jim Lee'],'Team Affliations':['X-Factor','X-Men','Marauders','Horsemen of Apocalypse,'],'Base of Operations':'Salem Centre, New York'}
+
+print(Freakzoid["Real Name"])
+print(Freakzoid.get("Origin", "Sorry we dont have that information"))
