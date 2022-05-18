13676c2f287bd8f6048cf4e59563003fc8cb7312
josermondragon <j.mondragon@hotmail.com>
2022-05-18 20:52:03 +0000

    dictionaries in python

--- superheroes.py
+++ superheroes.py
@@ -4,3 +4,18 @@ Freakzoid={'Real Name':'Remy LeBeau','First Appearance':'Uncanny X-Men Annual #1
 
 print(Freakzoid["Real Name"])
 print(Freakzoid.get("Origin", "Sorry we dont have that information"))
+print()
+print("the challenge starts here*****************************")
+print("original list: ", Freakzoid)
+Freakzoid["fav_food"] = "bandeja paisa"
+print("new list: ", Freakzoid)
+print("")
+print("all the keys: ", Freakzoid.keys())
+choice = input("choice a key from the last line of code in order to see its value: ")
+while choice not  in  Freakzoid.keys():
+    choice = input("do it again...Read!!")
+    if choice in Freakzoid.keys():
+    
+        break
+    else: print( "are you reading the F instructions? try again")
+print("Freakzoid ", choice, "is: ", Freakzoid[choice])   
