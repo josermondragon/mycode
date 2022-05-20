79dc7459bbc4d91e89ec285b5b3884fa76189dca
josermondragon <j.mondragon@hotmail.com>
2022-05-20 20:35:06 +0000

    Learning to parse log files

---
+++ textobjects.py
@@ -0,0 +1,16 @@
+#!/usr/bin/dev python3
+#from pprint import pprint
+
+#METHOD 1
+
+#foo= open("paradise.txt","a")
+
+#foo.write("JOSE YOU ARE DOING AWESOME\n")
+
+#print(foo.read())
+
+#pprint(foo.readlines())
+
+with open("paradise.txt") as x:
+    for line x:
+        print(line)
